adf4bf89b28a6760922a73cc15a3ad0f893f98fd
Frode Børli <frodeborli@gmail.com>
2015-02-02 12:11:21 +0100

    Cleaning up the server

--- server.php
+++ server.php
@@ -1,10 +1,6 @@
 <?php
 require 'vendor/autoload.php';
 
-$config = @parse_ini_file('config.ini', TRUE, INI_SCANNER_RAW);
-
-if(!$config) die("Error: No config.ini found!\n");
-
 spl_autoload_register(function($className) {
 	$path = str_replace("\\", "/", $className);
 	if(file_exists(__DIR__.'/classes/'.$path.'.class.php'))
